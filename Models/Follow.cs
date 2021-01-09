ca6470a2ecd73925d64730c490cfac36be95dfa4
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-09 18:00:00 +0300

    Added Auth on the photo create, edeit and delete

---
+++ Models/Follow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoApp.Models
+{
+    public class Follow
+    {
+        public int FollowId { get; set; }
+        public string Followed { get; set; }
+        public string following { get; set; }
+    }
+}
