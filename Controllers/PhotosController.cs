c61472b4aeb9f59ff866fb9a67944f7914608c9c
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-24 18:00:00 +0300

    Updating Delete

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -141,6 +141,17 @@ namespace PhotoApp.Controllers
             {
                 var followinglistID = new List<string>();
                 var allusersID = new List<string>();
+                foreach (var id in followering)
+                {
+                    followinglistID.Add(id.Following);
+                }
+
+                foreach (var id in users)
+                {
+                    users.Add(id);
+                }
+                var test = 
+
                 //foreach (var item in users)
                 //{ 
                 //    foreach (var items in followering)
