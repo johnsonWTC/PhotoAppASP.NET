6fb30d615aabe860f624d85cd906c96ceb97ca34
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-27 18:00:00 +0300

    Posts creation

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,5 +7,16 @@ namespace PhotoApp.Models
 {
     public class Comment
     {
+        public int commentID { get; set; }
+        public string commentContent { get; set; }
+
+        public int commentLikes { get; set; }
+
+        public int commentViews { get; set; }
+
+        public int postID { get; set; }
+
+        [JsonIgnore]
+        public Post post { get; set; }
     }
 }
