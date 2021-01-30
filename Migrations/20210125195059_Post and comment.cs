67d4eba227bc4430be3fe7d02e6cad78920ebc61
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-30 18:00:00 +0300

    Comments

---
+++ Migrations/20210125195059_Post and comment.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhotoApp.Migrations
+{
+    public partial class Postandcomment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Posts");
+        }
+    }
+}
