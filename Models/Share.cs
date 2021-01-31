cce942bbeb51f3260dc66dd8f6bcac7985cc8d7c
Johnson Dubula <jdubula@iqbusiness.net>
2021-01-31 18:00:00 +0300

    Adding share

--- Models/Share.cs
+++ Models/Share.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace PhotoApp.Models
 {
     public class Share
     {
+        [Key]
+        public int ShareId { get; set; }
+        public string PhotoOwner { get; set; }
+
+        public string PhotoLiker { get; set; }
+
+        public int PhotoId { get; set; }
+
+        public Photo Photo { get; set; }
     }
 }
